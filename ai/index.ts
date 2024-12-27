791f0d1568884c65ca577befc9d6753294731588
igna08 <156800096+igna08@users.noreply.github.com>
2024-12-27 18:13:11 -0300

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,7 +1,7 @@
 import { openai } from '@ai-sdk/openai';
 import { generateText } from 'ai';
 
-export const customModel = (apiIdentifier: string) => {
+export const { text }  = (apiIdentifier: string) => {
   return {
     model: openai(apiIdentifier),
   };
