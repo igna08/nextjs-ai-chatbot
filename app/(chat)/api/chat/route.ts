82fbca4256c08f27ae8a6ef3d7688135dd05bf89
igna08 <156800096+igna08@users.noreply.github.com>
2024-12-28 12:47:57 -0300

    Update route.ts

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -202,7 +202,7 @@ export async function POST(request: Request) {
           const { fullStream } = await streamText({
             model: customModel(model.apiIdentifier),
             system:
-              'You are a helpful legal writing assistant. Based on the description, please update the piece of writing.',
+              'You are a helpful legal writing assistant. Based on the description, please update the piece of writing and taking the data that he gave you.',
             experimental_providerMetadata: {
               openai: {
                 prediction: {
