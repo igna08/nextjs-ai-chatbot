b38711a37ea4b5e258e48759368eb27b06b1167c
igna08 <156800096+igna08@users.noreply.github.com>
2024-12-28 10:55:56 -0300

    Update custom-middleware.ts

--- ai/custom-middleware.ts
+++ ai/custom-middleware.ts
@@ -1,3 +1,21 @@
 import { Experimental_LanguageModelV1Middleware } from 'ai';
 
-export const customMiddleware: Experimental_LanguageModelV1Middleware = {};
+export const customMiddleware:  LanguageModelV1Middleware = {
+  transformParams: async ({ params }) => {
+    const lastUserMessageText = getLastUserMessageText({
+      prompt: params.prompt,
+    });
+
+    if (lastUserMessageText == null) {
+      return params; // do not use RAG (send unmodified parameters)
+    }
+
+    const instruction =
+      'Use the following information to answer the question:\n' +
+      findSources({ text: lastUserMessageText })
+        .map(chunk => JSON.stringify(chunk))
+        .join('\n');
+
+    return addToLastUserMessage({ params, text: instruction });
+  },
+};
