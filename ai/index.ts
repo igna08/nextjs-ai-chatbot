e98a07c498eecc3010283205a68d610744f8c523
igna08 <156800096+igna08@users.noreply.github.com>
2024-12-28 11:27:11 -0300

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,10 +1,10 @@
 import { openai } from '@ai-sdk/openai';
-import { Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware } from 'ai';
+import { Experimental_LanguageModelV1Middleware as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
 
 export const customModel  = (apiIdentifier: string) => {
-  return LanguageModelV1Middleware({
+  return wrapLanguageModel({
     model: openai(apiIdentifier),
     middleware: customMiddleware ,
   });
