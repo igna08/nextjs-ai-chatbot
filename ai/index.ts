77c6353fbc6f1f9fe5b67480168a0f33e6aff2b3
igna08 <156800096+igna08@users.noreply.github.com>
2025-01-05 15:46:20 -0300

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,10 +1,7 @@
 import { openai } from '@ai-sdk/openai';
-import { streamText } from 'ai';
 
 export const customModel = (apiIdentifier: string) => {
-  model: openai('gpt-4-turbo'),
-});
-
-for await (const textPart of textStream) {
-  console.log(textPart);
-}
+  return {
+    model: openai(apiIdentifier),
+  };
+};
