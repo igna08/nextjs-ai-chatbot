288c8f8424da9bf357ed9a3648365344261fb5df
igna08 <156800096+igna08@users.noreply.github.com>
2025-01-05 16:24:20 -0300

    Update actions.ts

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -2,6 +2,7 @@
 
 import { CoreMessage, CoreUserMessage, generateText } from 'ai';
 import { cookies } from 'next/headers';
+import { openai } from '@ai-sdk/openai';
 
 import { customModel } from '@/ai';
 
@@ -16,7 +17,7 @@ export async function generateTitleFromUserMessage({
   message: CoreUserMessage;
 }) {
   const { text: title } = await generateText({
-    model: customModel('gpt-3.5-turbo'),
+    model: openai('gpt-3.5-turbo'),
     system: `\n
     - you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
