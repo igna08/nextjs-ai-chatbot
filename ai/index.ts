874595863417f6c2095055b58920116667ddd5cc
igna08 <156800096+igna08@users.noreply.github.com>
2025-01-05 16:11:15 -0300

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -1,7 +1,8 @@
 import { openai } from '@ai-sdk/openai';
+import  {  models  }  de  '@/ai/models' ;
 
 export const customModel = (apiIdentifier: string) => {
   return {
-    model: openai(apiIdentifier),
+    model: openai( modelo.apiIdentifier ),
   };
 };
